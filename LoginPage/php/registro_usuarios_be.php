5d5b8e4ea4b49d82449993a0f2cf1c80ce38d386
Luciano Lemos <luciano.platzy@gmail.com>
2023-01-15 00:43:46 -0300

    Verificacion usuario repetido

--- LoginPage/php/registro_usuarios_be.php
+++ LoginPage/php/registro_usuarios_be.php
@@ -9,8 +9,25 @@
     $consulta = "INSERT INTO usuarios(nombre_completo, email, password)
               VALUES('$nombre_completo','$email','$password')";
 
-    $ejecutar = mysqli_query($conexion, $consulta);
 
+
+
+    // Verificar que no se repita el usuario
+    $verificar_correo = mysqli_query($conexion, "SELECT * FROM usuarios WHERE email='$email' ");
+
+    if(mysqli_num_rows($verificar_correo) > 0) {
+        echo '
+            <script>
+                alert("Este correo ya esta registrado. Por favor, intentelo con uno diferente");
+                window.location = "../index.php";
+            </script>
+            ';
+            exit();
+    }
+
+    // Mensaje una vez creado el usuario
+    $ejecutar = mysqli_query($conexion, $consulta);
+    
     if($ejecutar){
         echo '
             <script>
