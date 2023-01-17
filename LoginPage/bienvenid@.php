59edd60c7cdde64cac40255263cdd79b4e198533
Luciano Lemos <luciano.platzy@gmail.com>
2023-01-17 02:26:59 -0300

    Mostrar y ocultar pass con linterna

--- LoginPage/bienvenid@.php
+++ LoginPage/bienvenid@.php
@@ -1,6 +1,5 @@
 <?php
 
-
     session_start();
 
     //Comprobación de seguridad, solo pueden acceder usuarios que hayan iniciado sesión
@@ -16,9 +15,6 @@
         die();
     }
 
-
-
-
 ?>
 
 
@@ -27,35 +23,74 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <script src="https://kit.fontawesome.com/887a7d67c7.js" crossorigin="anonymous"></script>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <!-- https://cdnjs.com/libraries/font-awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Bienvenid@</title>
 </head>
 <body>
-    <div>
+    <div class="containerBienvenida">
         <div class="formulario">
             <div class="texto">
                 <h2>Bienvenido al fantástico mundo de Luciano Lemos</h2>
-                <p>Usted se encuentra dentro del usuario <?php echo $_SESSION['usuario']; ?></p>
+                <p>Usted se encuentra dentro del usuario</p>
+                <h4><?php echo $_SESSION['usuario'];?></h4>
+                <p>Su contraseña actual es:</p>
+                <div class="passContainer">
+                    <div class="containerLinterna">
+                        <div class="linterna off" id="linterna"></div>
+                        <!-- <button class="linterna" id="linterna"></button> -->
+                    </div>
+                    <!-- <div class="luciano"><i class="fa-duotone fa-flashlight luciano"></i></div> -->
+                    <div class="contrasenaActual" id="contrasenaActual">
+                        <h4><?php
+                            include 'php/conexion_be.php';
+
+                            // session_start();
+                            $usuario =  $_SESSION['usuario'];
+                            $consulta_vieja_contrasena = "SELECT contrasena FROM usuarios WHERE email = '$usuario'";
+
+
+                            // Obtiene la contraseña vieja y la crea en la variable contrasena_vieja
+                            
+                            $ejecutar = mysqli_query($conexion, $consulta_vieja_contrasena);
+
+                            if ($ejecutar) {
+                                while($row = $ejecutar->fetch_array()){
+                                    $contrasena_vieja = $row['contrasena'];
+                                }
+                            }
+
+                            echo $contrasena_vieja;
+                        ?></h4>
+                    </div>
+
+                </div>
             </div>
             <div class="botones">
                 <button class="botonLoco btnCambiarPass">Quieres cambiar la contraseña?</button>
                 <!-- <a href="" class="botonLoco btnCambiarPass">Quieres cambiar la contraseña?</a> -->
-                <a href="php/cerrar_sesion.php" class="botonLoco">Cerrar sesión</a>
+                <a href="php/cerrar_sesion.php" class="botonLoco">Cerrar <br> sesión</a>
 
             </div>
         </div>
         <div class="cambiarPass">
             <form action="php/cambio_password.php" method="POST" class="formulario">
-                <input type="password" placeholder="Contraseña anterior" name="vieja_password">
-                <input type="password" placeholder="Nueva contraseña" name="nueva_password">
+                <div class="containerCambioPass">
+                    <span class="eyeIcon"> <i class="fa-solid fa-eye-slash"></i> </span>
+                    <input type="password" placeholder="Contraseña anterior" name="vieja_password"> 
+                </div>
+                <div class="containerCambioPass">
+                    <span class="eyeIcon" id="eyeIcon2"> <i class="fa-solid fa-eye-slash"></i> </span>
+                    <input type="password" placeholder="Nueva contraseña" name="nueva_password">
+                </div>
                 <button class="botonLoco">Cambiar contraseña</button>
             </form>
         </div>
     </div>
-
 
 
     <script src="js/script.js"></script>
--- LoginPage/js/script.js
+++ LoginPage/js/script.js
@@ -22,3 +22,72 @@ document.addEventListener('click', e => {
         $cambiarPass .classList.toggle('active')
     }
 });
+
+
+
+// Mostrar contraseña y cambiar linterna
+var contrasenaActual = document.getElementById('contrasenaActual'),
+    btnlinterna =  document.getElementById('linterna'),
+    contador=0;
+
+function cambio ()
+    { if (contador==0)
+        {   
+            contrasenaActual.classList.add('active');
+            btnlinterna.classList.remove('off')
+            btnlinterna.classList.add('on')
+            contador=1;
+        }
+        else {
+            contrasenaActual.classList.remove('active');
+            btnlinterna.classList.remove('on')
+            btnlinterna.classList.add('off')
+            contador=0;
+        }
+    }
+
+btnlinterna.addEventListener('click', cambio, true);
+
+
+
+// Funciones eyes, ocultan y muestran las contraseñas
+// importante colocar el punto cuando lo buscamos por clase
+const eyeIcon = document.querySelector('.eyeIcon');
+const eyeIcon2 = document.getElementById("eyeIcon2");
+
+// corroboro si lo lee
+// console.log(eyeIcon);
+// console.log(eyeIcon2);
+
+eyeIcon.addEventListener('click', function(){
+    // creamos la constante del i (en caso contrario nos tomaria todo el span donde se encuentra)
+    const icon = this.querySelector("i");
+    // console.log(this.querySelector("i"));
+    // console.log(this);
+    // console.log(this.nextElementSibling.type); this muestra donde estoy, nextelement.. muestra el siguiente (en este caso el input), type el tipo del input
+    if (this.nextElementSibling.type === "password") {
+        this.nextElementSibling.type = "text";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }else{
+        this.nextElementSibling.type = "password";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    }
+})
+eyeIcon2.addEventListener('click', function(){
+    // creamos la constante del i (en caso contrario nos tomaria todo el span donde se encuentra)
+    const icon = this.querySelector("i");
+    // console.log(this.querySelector("i"));
+    // console.log(this);
+    // console.log(this.nextElementSibling.type); this muestra donde estoy, nextelement.. muestra el siguiente (en este caso el input), type el tipo del input
+    if (this.nextElementSibling.type === "password") {
+        this.nextElementSibling.type = "text";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }else{
+        this.nextElementSibling.type = "password";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    }
+})
