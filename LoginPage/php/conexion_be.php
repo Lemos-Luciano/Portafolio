243ff337d877b46a5dc7e18d894013104aa1ec3c
Luciano Lemos <luciano.platzy@gmail.com>
2023-01-18 00:27:44 -0300

    login Page conf host remoto

--- LoginPage/php/conexion_be.php
+++ LoginPage/php/conexion_be.php
@@ -1,7 +1,22 @@
 <?php
 
-    $conexion = mysqli_connect("localhost", "root", "", "login_register");
+    // CONEXION EN LOCAL 
+    // $conexion = mysqli_connect("localhost", "root", "", "login_register");
 
+
+    // CONEXION A WEBHOST
+    // Nombre de la Base de Datos
+    // id20165373_login_register	
+    // Usuario de la Base de Datos
+    // id20165373_lemosluciano	
+    // contraseña
+    // R%J)U\{>=8)p4m6C
+
+    $conexion = mysqli_connect("localhost", "id20165373_lemosluciano", "R%J)U\{>=8)p4m6C", "id20165373_login_register");
+
+
+
+    // Verifica si conecta
     // if($conexion){
     //     echo "conectado exitosamente a la base de datos";
     // }else{
