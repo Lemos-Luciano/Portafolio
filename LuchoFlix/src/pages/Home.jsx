f7d7ea2abee984a40286649c377612fec61d6aab
Luciano Lemos <luciano.platzy@gmail.com>
2023-03-11 22:22:02 +0000

    home isScrolled

--- LuchoFlix/src/pages/Home.jsx
+++ LuchoFlix/src/pages/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // Si se realiza scroll se ejecuta ???
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
   return (
     <div>
-      Home
+      <Navbar isScrolled={isScrolled} />
     </div>
-  )
+  );
 }
