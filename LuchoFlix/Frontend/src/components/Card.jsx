aa6f103625b36cdf50d8b4a76d9c572d1975f8d5
Luciano Lemos <luciano.platzy@gmail.com>
2023-04-13 03:55:25 +0100

    fixed autoplay

--- LuchoFlix/Frontend/src/components/Card.jsx
+++ LuchoFlix/Frontend/src/components/Card.jsx
@@ -95,7 +95,7 @@ function Card({ movieData, isLiked = false }) {
         <br>
         <h5> ${movieData.genres} </h5>
         <iframe
-          src="http://www.youtube.com/embed/${movieData.trailer}?autoplay=1&mute=1"
+          src="http://www.youtube.com/embed/${movieData.trailer}"
           frameborder="0"
         /> 
 
