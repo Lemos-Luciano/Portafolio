00f5f5a4cfa376b9b3490f2741b5e192f11a2251
Luciano Lemos <luciano.platzy@gmail.com>
2023-05-10 19:53:03 +0100

    Hero section

--- personalweb_2.0/app/components/BackgroundCircles.tsx
+++ personalweb_2.0/app/components/BackgroundCircles.tsx
@@ -14,7 +14,7 @@ function BackgroundCircles({}: Props) {
     transition={{ duration: 2.5,}}
 
     className='relative flex justify-center items-center'>
-        <div className='border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
+        <div className='border border-[#333333] rounded-full h-[400px] w-[400px] mt-52 animate-ping absolute'/>
         <div className='border border-[#333333] rounded-full h-[300px] w-[300px] mt-52 absolute'/>
         <div className='border border-[#333333] rounded-full h-[500px] w-[500px] mt-52 absolute'/>
         <div className='rounded-full border border-[#F7AB0A] opacity-20 h-[650px] w-[650px] absolute mt-52 animate-pulse' />
--- personalweb_2.0/app/components/Hero.tsx
+++ personalweb_2.0/app/components/Hero.tsx
@@ -1,28 +1,53 @@
-'use client'
-import React from 'react';
-import { Cursor, useTypewriter } from 'react-simple-typewriter';
-import BackgroundCircles from './BackgroundCircles';
+"use client";
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import Link from "next/link";
 
-type Props = {}
+type Props = {};
 
 export default function Hero({}: Props) {
-    const [text, count] = useTypewriter({
-        words: [
-            "Luciano Lemos",
-            "Quieres contactarme",
-            "Bienvenid@s"
-        ],
-        loop: true,
-        delaySpeed: 2000,
-    })
+  const [text, count] = useTypewriter({
+    words: [
+      "Luciano Lemos",
+      "Saltando el renglon si el texto es largo como este ejemplo",
+      "Bienvenid@s",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
   return (
-    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
-        <BackgroundCircles />
-        <img src="" alt="" />
-        <h1>
-            <span> {text}</span>
-            <Cursor cursorColor='#F7AB0A'/>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://github.com/Lemos-Luciano/Portafolio/blob/main/Images/CV/fotoCV.jpg?raw=true"
+        alt="Profile Image"
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Desarrollador Fullstack
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span> {text} </span>
+          <Cursor cursorColor="#F7AB0A" />
         </h1>
+
+        <div className="pt-5">
+          <Link href="about">
+            <button className="heroButton"> About </button>
+          </Link>
+          <Link href="experiencia">
+            <button className="heroButton"> Experiencia </button>
+          </Link >
+          <Link href="skills">
+            <button className="heroButton"> Skills </button>
+          </Link>
+          <Link href="proyectos">
+            <button className="heroButton"> Proyectos </button>
+          </Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
