fa83f586731603c237f4bdb964cf18d254e26be1
Luciano Lemos <luciano.platzy@gmail.com>
2023-05-12 15:18:59 +0100

    diseño submit

--- personalweb_2.0/app/components/ContactMe.tsx
+++ personalweb_2.0/app/components/ContactMe.tsx
@@ -1,20 +1,10 @@
 "use client";
 import React from 'react';
 import { BeakerIcon, EnvelopeIcon, MapPinIcon, PhoneIcon } from '@heroicons/react/24/solid';
-// import { SubmitHandler, useForm } from "react-hook-form";
-
-// type Inputs = {
-//     name: string,
-//     email: string,
-//     asunto: string,
-//     mensaje: string,
-// };
 
 type Props = {}
 
 export default function ContactMe({}: Props) {
-    //   const { register, handleSubmit } = useForm<Inputs>();
-    //   const onSubmit : SubmitHandler<Inputs> = (Formdata) => console.log(Formdata);
   return (
     <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
@@ -23,7 +13,7 @@ export default function ContactMe({}: Props) {
 
         <div className='flex flex-col space-y-10'>
             <h4 className='text-4xl font-semibold text-center'>
-                Tengo justo lo que usted necesita. 
+                Tengo justo lo que usted necesita.
                 <span className='decoration-[#F7AB0A]/50 underline'>Contácteme</span>
             </h4>
 
@@ -44,29 +34,23 @@ export default function ContactMe({}: Props) {
                 </div>
             </div>
 
-            {/* <form className='flex flex-col space-y-2 w-fit mx-auto'>
+            <form action="https://formsubmit.co/llemos@uoc.edu" method="POST"
+            className='flex flex-col space-y-2 w-fit mx-auto'>
                 <div className='flex space-x-2'>
-                    <input placeholder='Nombre' className="contactInput" type="text" />
-                    <input placeholder='Email' className="contactInput" type="email" />
+                    <input type="text" placeholder="Nombre" name="nombre" className="contactInput"  />
+                    <input type="email" placeholder="email"  name="email" className="contactInput"  />
                 </div>
-
-                <input placeholder='Asunto' className="contactInput" type="text" />
-
-                <textarea placeholder='Mensaje' className="contactInput"/>
-                <button 
-                type="submit"
-                className='bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold'>Enviar</button>
-            </form> */}
-
-            
-            <form action="https://formsubmit.co/llemos@uoc.edu" method="POST">
-                <input type="text" placeholder="Nombre" name="nombre" />
-                <input type="email" placeholder="email"  name="email" />
-                <input type="text" placeholder="Motivo" name="motivo" />
-                <textarea name="mensaje" placeholder="Mensaje"></textarea>
-                <button 
-                type="submit"
-                className='bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold'>Enviar</button>            </form>
+                <input type="text" placeholder="Motivo" name="motivo"  className="contactInput"  />
+                <textarea name="mensaje" placeholder="Mensaje"  className="contactInput" ></textarea>
+                <button
+                    type="submit"
+                    className='bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold'>Enviar
+                </button>
+                {/* <!-- Luego de submit, nos dirige a la pagina deseada --> */}
+                <input type="hidden" name="_next" value="https://lucianolemos.vercel.app/"/>
+                {/* <!-- Quita el captcha de formsumit -->   */}
+                <input type="hidden" name="_captcha" value="false"/>
+            </form>
         </div>
     </div>
   )
