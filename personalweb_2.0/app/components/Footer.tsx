cd76519986fb18bdd30314e796fdacf5250b2f38
Luciano Lemos <llemos@uoc.edu>
2025-03-08 09:40:39 +0100

    fix Bug one div father

--- personalweb_2.0/app/components/Footer.tsx
+++ personalweb_2.0/app/components/Footer.tsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React from 'react';
 
-type Props = {}
+type Props = {};
 
 export default function Footer({}: Props) {
   return (
-    <div className="sticky top-[90vh] max-md:top-[85vh] w-full cursor-pointer flex items-center justify-center mx-auto z-20 ">
-        <a href="#hero" >
+    <div className="sticky top-[90vh] max-md:top-[85vh] w-full cursor-pointer flex flex-col items-center justify-center mx-auto z-20">
+      {/* Enlace con imagen */}
+      <a href="#hero">
         <div className="flex items-center justify-center">
-            <img src="../../DedoPaArriba.png"
+          <img
+            src="../../DedoPaArriba.png"
             alt="Imagen Proyecto"
-            className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 hover:scale-150 cursor-pointer" />
+            className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 hover:scale-150 cursor-pointer"
+          />
         </div>
-    </a>
-  </div>
-  <div> 
-        <iframe title="Jerarquias2" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=0dd75a14-0b20-4ea4-9642-93f00d64d437&autoAuth=true&ctid=3a6d51bf-f1d4-47df-9a61-8bbe9c0e1e66" frameborder="0" allowFullScreen="true"></iframe>
+      </a>
+
+      {/* Contenedor del iframe */}
+      <div className="mt-4"> 
+        <iframe
+          title="Jerarquias2"
+          width="1140"
+          height="541.25"
+          src="https://app.powerbi.com/reportEmbed?reportId=0dd75a14-0b20-4ea4-9642-93f00d64d437&autoAuth=true&ctid=3a6d51bf-f1d4-47df-9a61-8bbe9c0e1e66"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
-  )
+  );
 }
